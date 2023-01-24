ba157d84fbe4bfa5ce3d64c2434be4d21f806607
Roman Isopenko <isopenkoroman@gmail.com>
2023-01-24 18:59:35 +0100

    Add new vocab for Mike for practice

--- js/data.js
+++ js/data.js
@@ -1,38 +1,30 @@
 export default [
   {
-    term: "attic",
-    hint: "the space or room at the top of a building, under the roof, often used for storing things",
+    term: "commute",
+    hint: "to get from home to your workplace",
   },
   {
-    term: "ceiling",
-    hint: "the inside surface of a room that you can see when you look above you",
+    term: "self-sufficient,",
+    hint: "able to provide everything you need, especially food, for yourself without the help of other people",
   },
   {
-    term: "cellar",
-    hint: "a room under the ground floor of a building, usually used for storing things",
+    term: "get rid of,",
+    hint: "to remove something that you do not want any longer",
   },
   {
-    term: "neighborhood",
-    hint: "the area of a town that surrounds someone's home, or the people who live in this area",
+    term: "dull,",
+    hint: "boring",
   },
   {
-    term: "suburb",
-    hint: "an area on the edge of a large town or city where people who work in the town or city often live",
+    term: "frame,",
+    hint: "it is taken by a photo camera",
   },
   {
-    term: "handkerchief",
-    hint: "a square piece of cloth or paper used for cleaning the nose or drying the eyes when they are wet with tears",
+    term: "blanket,",
+    hint: "your cover yourself with it while sleeping",
   },
   {
-    term: "picturesque",
-    hint: "a square piece of cloth or paper used for cleaning the nose or drying the eyes when they are wet with tears",
-  },
-  {
-    term: "yurt",
-    hint: "a type of round tent with a wooden frame, used traditionally as a home by some Central Asian people, and now sometimes used for camping in Western countries",
-  },
-  {
-    term: "central heating",
-    hint: "a system of heating buildings by warming air or water at one place and then sending it to different rooms in pipes",
+    term: "speechless,",
+    hint: "unable to utter any word",
   },
 ];
